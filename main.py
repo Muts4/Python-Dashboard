c927d9fd37859e6fe1080c13b0cad9906927f9d2
Matt Metz <mattmetz25@gmail.com>
2024-01-12 13:29:29 -0600

    NBA api example added

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ frame2.place(relx=0.5, rely=0.0, relwidth=0.5, relheight=0.5)
 frame3.place(relx=0, rely=0.5, relwidth=0.5, relheight=0.5)
 frame4.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.5)
 
-# Create content for each quadrant
+# # Create content for each quadrant
 label1 = tk.Label(frame1, text="Stock Ticker", font=("Helvetica", 12))
 label1.pack(pady=10)
 
@@ -49,7 +49,6 @@ def stock_ticker():
     try:
         r = requests.get(url)
         data = r.json()
-        print(r.text)
         latest_timestamp = max(data['Time Series (5min)'], key=lambda x: x)
         stock_price = data['Time Series (5min)'][latest_timestamp]['4. close']
 
@@ -63,8 +62,41 @@ def stock_ticker():
 stock_ticker()
 
 
-label2 = tk.Label(frame2, text="Quadrant 2", font=("Helvetica", 12))
+label2 = tk.Label(frame2, text="Bulls Record", font=("Helvetica", 12))
 label2.pack(pady=10)
+
+def bulls_record():
+    nba_endpoint = 'https://www.balldontlie.io/api/v1/teams'
+    bulls_id = 5
+    url1 = f'{nba_endpoint}/{bulls_id}'
+    response = requests.get(url1)
+
+    try:
+        response.raise_for_status()
+
+        # Parse the JSON response
+        data = response.json()
+
+        # Extract relevant information (e.g., win and loss records)
+        full_name = data['full_name']
+
+        label2.config(text=f"Team: {full_name}")
+
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+
+    label2.after(3600000, bulls_record)
+
+# Call the function to get and print the Bulls' record
+bulls_record()
+
+
 
 label3 = tk.Label(frame3, text="Quadrant 3", font=("Helvetica", 12))
 label3.pack(pady=10)
